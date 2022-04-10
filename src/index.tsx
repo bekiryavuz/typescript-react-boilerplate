cdb4cdcdc7a8f7325381c9d01586d2ca569e73d9
Bekir Yavuz <byculs@gmail.com>
2022-04-10 12:56:58 +0300

    Initial empty commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as ReactDOM from "react-dom";
+import "../src/style.css";
+
+function App() {
+  return (
+    <div>Lets Start! </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
